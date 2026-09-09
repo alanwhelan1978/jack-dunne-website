11fdb032589d5706dc288bbc489044bbe68477af
alanwhelan1978 <89021083+alanwhelan1978@users.noreply.github.com>
2026-09-09 22:46:17 +0100

    Add menu toggle functionality for navigation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 
+const menuButton = document.querySelector(".menu-toggle");
+const navigation = document.querySelector(".main-nav");
+
+if (menuButton && navigation) {
+  menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+  });
+}
